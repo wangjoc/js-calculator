764befcb39761c42fa337c2abecdae2fe7bd276f
Jocelyn Wang <wangjoc@gmail.com>
2020-03-31 08:30:52 -0700

    completed base case logic and updates to base case examples

--- calculator.js
+++ calculator.js
@@ -3,47 +3,80 @@
 const exampleAdditionInput = {
   num1: 3,
   num2: 5,
-  operation: 'add',
+  op: 'add',
 }
 
 const exampleAdditionInputSign = {
   num1: 3,
   num2: 5,
-  operation: '+',
+  op: '+',
 }
 
 const exampleSubtractInput = {
   num1: 10,
   num2: 5,
-  operation: 'subtract',
+  op: 'subtract',
 }
 
 const exampleSubtractInputSign = {
   num1: 10,
   num2: 5,
-  operation: '-',
+  op: '-',
 }
 
 const exampleMultiInput = {
   num1: 3,
   num2: 5,
-  operation: 'multiply',
+  op: 'multiply',
 }
 
 const exampleMultiInputSign = {
   num1: 3,
   num2: 5,
-  operation: '*',
+  op: '*',
 }
 
 const exampleDivideInput = {
   num1: 3,
   num2: 5,
-  operation: 'divide',
+  op: 'divide',
 }
 
 const exampleDivideInputSign = {
   num1: 3,
   num2: 5,
-  operation: '/',
+  op: '/',
 }
+
+/// CALCULATOR FUNCTIONALITY ///
+
+function calculator(data) {
+  if (data.op === 'add' || data.op === '+') {
+    return data.num1 + data.num2
+  } 
+  else if (data.op === 'subtract' || data.op === '-') {
+    return data.num1 - data.num2
+  } 
+  else if (data.op === 'multiply' || data.op === '*') {
+    return data.num1 * data.num2
+  } 
+  else if (data.op === 'divide' || data.op === '/') {
+    return data.num1 / data.num2
+  } 
+} 
+
+
+/// BASE CASE EXAMPLES ///
+
+console.log("*** ADDITION ***")
+console.log(calculator(exampleAdditionInput));
+console.log(calculator(exampleAdditionInputSign));
+console.log("*** SUBTRACTION ***")
+console.log(calculator(exampleSubtractInput));
+console.log(calculator(exampleSubtractInputSign));
+console.log("*** MULTIPLICATION ***")
+console.log(calculator(exampleMultiInput));
+console.log(calculator(exampleMultiInputSign));
+console.log("*** DIVIDE ***")
+console.log(calculator(exampleDivideInput));
+console.log(calculator(exampleDivideInputSign));
