851cf75172c1615cb7c58bd5c58ec8b1d950aeee
Jocelyn Wang <wangjoc@gmail.com>
2020-03-31 08:19:13 -0700

    finished putting together all sample inputs

--- calculator.js
+++ calculator.js
@@ -1,5 +1,49 @@
+/// SAMPLE INPUTS ///
+
 const exampleAdditionInput = {
   num1: 3,
   num2: 5,
   operation: 'add',
+}
+
+const exampleAdditionInputSign = {
+  num1: 3,
+  num2: 5,
+  operation: '+',
+}
+
+const exampleSubtractInput = {
+  num1: 10,
+  num2: 5,
+  operation: 'subtract',
+}
+
+const exampleSubtractInputSign = {
+  num1: 10,
+  num2: 5,
+  operation: '-',
+}
+
+const exampleMultiInput = {
+  num1: 3,
+  num2: 5,
+  operation: 'multiply',
+}
+
+const exampleMultiInputSign = {
+  num1: 3,
+  num2: 5,
+  operation: '*',
+}
+
+const exampleDivideInput = {
+  num1: 3,
+  num2: 5,
+  operation: 'divide',
+}
+
+const exampleDivideInputSign = {
+  num1: 3,
+  num2: 5,
+  operation: '/',
 }
